5d1bb864927cad7b964b5ce7f9874e9749b752d8
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-05-18 20:49:47 +0700

    edit search_by_title

--- src/models/modules/search_books_by_title/search_books_by_title.py
+++ src/models/modules/search_books_by_title/search_books_by_title.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from difflib import get_close_matches
 
+
 class SearchBooksByTitle:
     def __init__(self, library_path):
-        self.dataset_df = pd.read_csv(library_path)  # ← правильный способ загрузки CSV
-        self.titles = self.dataset_df["Title"].dropna().to_list()
+        self.dataset_df = pd.read_csv(library_path)
+        self.titles = self.dataset_df["Title"].dropna().tolist()
 
-    def closest_title(self, title, size):
-        return get_close_matches(title, self.titles, n=size, cutoff=0.5)
+    def closest_title(self, title, size=5):
+        result = [tit for tit in self.titles if title.lower() in tit.lower()]
+
+        similar = get_close_matches(title, self.titles, n=size, cutoff=0.5)
+
+        combined = list(dict.fromkeys(result + similar))
+        return combined
